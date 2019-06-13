0d5f1a819ff2ec6d9ba7b3c969cfddedfde86895
FlorisFok <43857896+FlorisFok@users.noreply.github.com>
2019-06-13 16:24:43 +0200

    log update

--- static/map.js
+++ static/map.js
@@ -1,16 +1,16 @@
 
 var margin = {top: 40, right: 40, bottom: 40, left: 40};
 
-var svg = d3.select("#map"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+var svgmap = d3.select("#map"),
+    width = +svgmap.attr("width"),
+    height = +svgmap.attr("height");
 
 // Should really change this to 'clipExtent' instead of center
 var projection = d3.geoAlbers()
   .center([4.9, 52.366667])
   .parallels([51.5, 51.49])
   .rotate(120)
-  .scale(90000)
+  .scale(200000)
   .translate([width / 2, height / 2]);
 
 var path = d3.geoPath()
@@ -22,7 +22,7 @@ var colorScale = d3.scaleOrdinal(d3.schemeCategory20)
     colorStadsdelen = d3.scaleOrdinal(d3.schemePastel2); //d3.schemeGreys
     colorLines = d3.scaleSequential(d3.schemeCategory20);
 
-svg.append("text")
+svgmap.append("text")
   .attr("x", 0)
   .attr("y", 15)
   .attr("font-size", "large")
@@ -35,43 +35,84 @@ var spacingy = 20
 var x0 = 5
 var spacingx = 55
 
-/*Legenda*/
-svg.append("circle")
-  .attr("class", "station")
-  .attr("cx", x0 + 22)
-  .attr("cy", y0 + spacingy * 3)
-  .attr("r", 3);
-svg.append("text")
-  .attr("class", "label")
-  .attr("x", spacingx + 5)
-  .attr("y", y0 + spacingy * 3 + 5)
-  .text("Kamers");
+// /*Legenda*/
+// svgmap.append("circle")
+//   .attr("class", "station")
+//   .attr("cx", x0 + 22)
+//   .attr("cy", y0 + spacingy * 3)
+//   .attr("r", 3);
+// svgmap.append("text")
+//   .attr("class", "label")
+//   .attr("x", spacingx + 5)
+//   .attr("y", y0 + spacingy * 3 + 5)
+//   .text("Kamers");
 
+function colorPath(d) {
+  g.selectAll('path').style('fill', 'grey')
+  if (d.depth == 3){
+    document.querySelector("#sunpath").innerHTML =
+   `${d.parent.parent.data.name} -->
+   ${d.parent.data.name}
+   --> ${d.data.name}`
+   document.querySelector(`.${d.parent.parent.data.name.replace(/ /g,".")}`).style = "fill: red;"
+   document.querySelector(`.${d.parent.data.name.replace(/ /g,".")}`).style = "fill: red;"
+   document.querySelector(`.${d.data.name.replace(/ /g,".")}`).style = "fill: red;"
+  }
+  else if (d.depth == 2){
+    document.querySelector("#sunpath").innerHTML =
+   `${d.parent.data.name}
+   --> ${d.data.name}`
+   document.querySelector(`.${d.parent.data.name.replace(/ /g,".")}`).style = "fill: red;"
+   document.querySelector(`.${d.data.name.replace(/ /g,".")}`).style = "fill: red;"
+  }
+  else if (d.depth == 1){
+    document.querySelector("#sunpath").innerHTML =
+   `${d.data.name}`
+   document.querySelector(`.${d.data.name.replace(/ /g,".")}`).style = "fill: red;"
+  }
+}
+
+function inside_poly(point, pol) {
+    var x = point[0], y = point[1];
+    var inside = false;
+    for (var i = 0, j = pol.length - 1; i < pol.length; j = i++) {
+        var xi = pol[i][0], yi = pol[i][1];
+        var xj = pol[j][0], yj = pol[j][1];
+
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+
+    return inside;
+};
 
 d3.queue()
     .defer(d3.json, "../data/GEBIED_BUURTEN.json")
-    // .defer(d3.json, "trammetro.json")
+    .defer(d3.json, "../data/apidata2.json")
     .defer(d3.json, "../data/trammetrostations.geojson")
-    // .defer(d3.json, "spoor.geojson")
     .defer(d3.csv,  "../data/treinstations.csv")
     .await(ready);
 
-function ready(error, buurten, trammetrostations, treinstations) {
+function ready(error, buurten, api_data, trammetrostations, treinstations) {
   if (error) throw error;
 
   /* Areas */
+  console.log(buurten)
 
   var stadsdelen = buurten.features//topojson.feature(buurten, buurten)//.features;
 
   // Draw the buurten
-  svg.selectAll(".buurt")
+  svgmap.selectAll(".buurt")
       .data(stadsdelen)
     .enter().insert("g")
       .append("path")
         .attr("class", "buurt")
         .attr("d", path)
         // .attr("fill", "#faebc4")
-        .attr("fill", function(d) { return colorStadsdelen(d.properties.Stadsdeel_code[0]) })
+        .attr("fill", function(d) { if (d.properties.Buurtcombinatie_code == 'N73'){return "white"}
+                                    else if (d.properties.Stadsdeel_code == 'B'){return "white"}
+                                    else{return colorStadsdelen(d.properties.Stadsdeel_code[0]) }})
         .on("click", function(d) {
             var html = "";
 
@@ -83,6 +124,9 @@ function ready(error, buurten, trammetrostations, treinstations) {
             html += `${d.properties.Buurt}`
             html += "</span>";
             html += "</div>";
+
+            d = document.querySelector(`.${d.properties.Buurt.replace(/ /g,".")}`).__data__
+            colorPath(d)
 
             if (!($(".tooltip_key").text().substr(0,4) ==
                     d.properties.Buurt.substr(0,4))){
@@ -113,42 +157,25 @@ function ready(error, buurten, trammetrostations, treinstations) {
       .append("title")
         .text(function(d) { return stadsdeel[d.properties.Stadsdeel_code] + ": " + d.properties.Buurt });
 
-  console.log(buurten.features[0])
   // Draw borders around buurten
-  svg.append("path")
+  svgmap.append("path")
       .data(stadsdelen)
       .enter().insert("g")
       .attr("class", "buurt-borders")
       .attr("d", path)
-      // .attr("fill", "#faebc4")
       .attr("style", "outline: solid black;")
       .style("fill", "none")
     .append("title")
-      // .text(function(d) { return stadsdeel[d.properties.Stadsdeel_code] + ": " + d.properties.Buurtcombinatie });
 
   /* Points */
-
   // Draw the points for the stations
-  var station = svg.selectAll(".treinstations")
-    .data(treinstations)
+  var station = svgmap.selectAll(".treinstations")
+    .data(api_data.data)
     .enter().append("circle")
-      .attr("transform", function(d) { return "translate(" + projection([d["0"], d["1"]]) + ")"; })
-      .attr("class", "station");
+      .attr("transform", function(d) { return "translate(" + projection([d["lon"], d["lat"]]) + ")"; })
+      .attr("class", "station")
+      .attr("r", 3)
+      .style('fill',function(d) {if (d.tijd == -1){return 'green'} else{return 'red'}});
 
-  // // Draw name next to station point
-  // svg.selectAll(".stationnaam")
-  //     .data(treinstations)
-  //     .enter().append("text")
-  //       .text(function(d) { return d.name; }) //.split(" ")
-  //       .attr("class", "stationnaam")
-  //       .attr("x", function(d) { return projection([d["0"], d["1"]])[0] - d.name.length/3})
-  //       .attr("y", function(d) { return projection([d["0"], d["1"]])[1] - 7})
-
-  // Draw the points for the stations
-  svg.selectAll(".trammetrostations")
-    .data(trammetrostations.features)
-    .enter().append("circle")
-      .attr("transform", function (d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
-      .attr("class", function (d) { return "halte " + d.properties.Modaliteit.toLowerCase() });
 
 };
