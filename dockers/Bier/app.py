1a48a6b3c5a8835fd593d872fa4cc51c5889f1d6
FlorisFok <43857896+FlorisFok@users.noreply.github.com>
2019-06-15 22:45:31 +0200

    pils

---
+++ dockers/Bier/app.py
@@ -0,0 +1,116 @@
+from flask import Flask, redirect, render_template, request, jsonify
+import json
+from model import add_beer, get_all, add_krat, add_stat, get_names
+
+
+app = Flask(__name__)
+
+###############################################################################
+beers =  range(1,25)
+kratten = range(1,11)
+
+## Select backend
+# MODE = "CSV"
+MODE = "google"
+
+
+print(f"Back-end is using {MODE}")
+
+temp_beer = 0
+temp_name = ''
+###############################################################################
+
+# Reload templates when they are changed
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+@app.after_request
+def after_request(response):
+    """Disable caching"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    names = get_names()
+
+    return render_template("index2.html", beers=beers, kratten = kratten, names=names)
+
+@app.route("/krat")
+def krat():
+    return render_template("krat.html", beers=beers, kratten = kratten)
+
+@app.route("/registrants")
+def registrants():
+    # Get some nice stats to show
+    mens_list, beer_list = get_all()
+
+    # Transfom stats to JSON
+    i = 1
+    users = []
+    users2 = []
+    for mens, beer in zip(mens_list, beer_list):
+        try:
+            users.append({"y": beer, "label": mens})
+            users2.append({"y": (beer/sum(beer_list))*100, "label": mens})
+        except:
+            pass
+        i += 1
+
+    # Return values for JavaScript Table
+    return render_template("stats.html", users=json.dumps(users), users2=json.dumps(users2))
+
+
+@app.route("/", methods=["POST"])
+def index_back():
+    return render_template("index2.html", beers=beers, kratten = kratten)
+
+
+@app.route("/registrants", methods=["POST"])
+def registrants_stats():
+    return redirect("/registrants")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+
+    # Get all the forms
+    temp_name = request.form.get("name")
+    temp_beer = request.form.get("biertjes")
+    temp_krat = request.form.get("kratten")
+    temp_nivo = request.form.get("kwali")
+    temp_stat = request.form.get("kratten2")
+
+    # Check if they are filled in, takes action depending on mode
+    if not temp_name:
+        return render_template("failure.html", message = 'Please enter a name')
+
+    elif not temp_beer and not temp_krat and not temp_nivo and not temp_stat:
+        return render_template("failure.html",
+               message = 'Please enter a a number of crates or bottles')
+
+    elif not temp_krat and not temp_stat:
+        if not temp_nivo:
+            return render_template("failure.html",
+                   message = 'Please enter the quality of the beer')
+
+        add_beer(temp_name, temp_beer, temp_nivo)
+
+
+    elif not temp_stat:
+        if not temp_nivo:
+            return render_template("failure.html",
+                   message = 'Please enter the quality of the beer')
+
+        add_krat(temp_name, temp_nivo, temp_krat)
+
+
+    else:
+        add_stat(temp_name, temp_stat)
+
+    return redirect("/registrants")
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
---
+++ dockers/Bier/model.py
@@ -0,0 +1,180 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import time
+
+MENS_COL = 1
+BIER_COL = 2
+PILS_COL = 3
+TIME_COL = 4
+CREDIT_COL = 5
+STAT_COL = 6
+
+STATIE = 3.90
+BOTTLES = 24
+# Gives some flexability to small price changes
+PRICE_BARRIER = 0.5
+
+def use_sheet(name = 'Test'):
+	'''
+	Connect with api
+	'''
+	scope = ['https://spreadsheets.google.com/feeds',
+	         'https://www.googleapis.com/auth/drive']
+
+	creds = ServiceAccountCredentials.from_json_keyfile_name('Database-6141813cd99e.json', scope)
+	client = gspread.authorize(creds)
+
+	sheet  = client.open(name).sheet1
+
+	return sheet
+
+
+def set_up(sheet):
+	'''
+	Set up, if got empty sheet (mannually activation)
+	usage:
+	cmd: ...path_to_file>python
+	import mdoel
+	sheet = model.use_sheet("name")
+	set_up(sheet)
+
+	OR
+
+	Run as main
+	'''
+	try:
+		sheet.update_cell(1, MENS_COL, 'Mensen')
+		sheet.update_cell(1, BIER_COL, 'Lager')
+		sheet.update_cell(1, PILS_COL, 'Pils')
+		sheet.update_cell(1, TIME_COL, 'Laatste biertje')
+		sheet.update_cell(1, CREDIT_COL, 'Biertjes tegoed')
+	except:
+		return 1
+	return 0
+
+
+def add_beer(mens, biertjes, niveau):
+	'''
+	Mens (str), stuff (list), finds row of mens, adds stuff
+	'''
+	sheet = use_sheet()
+
+	row_num = find_mens_row(mens, sheet)
+
+	if float(niveau) < PRICE_BARRIER:
+		update(sheet, row_num, mens, BIER_COL, int(biertjes))
+	else:
+		update(sheet, row_num, mens, PILS_COL, int(biertjes))
+
+	tijd = time_stamp()
+	sheet.update_cell(row_num, TIME_COL, tijd)
+
+	return 0
+
+
+def add_krat(mens, niveau, krat):
+	'''
+	Add krat
+	'''
+	sheet = use_sheet()
+	row_num = find_mens_row(mens, sheet)
+	update(sheet, row_num, mens, CREDIT_COL,
+		   int(krat)*BOTTLES*float(niveau))
+
+	return 0
+
+
+def add_stat(mens, stat):
+	'''
+	Add statiegeld
+	'''
+	sheet = use_sheet()
+	row_num = find_mens_row(mens, sheet)
+	update(sheet, row_num, mens, STAT_COL, int(stat)*STATIE)
+
+	return 0
+
+
+def get_all():
+	'''
+	Get all beer consumed, combined both cheap and premium pils
+	Can be used for future projects to track alc. %.
+	'''
+	sheet = use_sheet()
+
+	mens_list = sheet.col_values(MENS_COL)
+	beer_list1 = sheet.col_values(BIER_COL)
+	beer_list2 = sheet.col_values(PILS_COL)
+
+	beer_list = []
+
+	for b,b2 in zip(beer_list1[1:], beer_list2[1:]):
+		try:
+			beer_list.append(int(b)+int(b2))
+		except:
+			beer_list.append(0)
+
+	return mens_list[1:], beer_list
+
+def get_names():
+	'''
+	Get all beer consumed, combined both cheap and premium pils
+	Can be used for future projects to track alc. %.
+	'''
+	sheet = use_sheet()
+	mens_list = sheet.col_values(MENS_COL)
+
+	return mens_list[1:]
+
+def null(sheet,mens,row_num):
+	"""
+	Makes sure you start with empty row
+	"""
+	sheet.update_cell(row_num, BIER_COL, 0)
+	sheet.update_cell(row_num, PILS_COL, 0)
+	sheet.update_cell(row_num, CREDIT_COL, 0)
+	sheet.update_cell(row_num, STAT_COL, 0)
+
+
+def update(sheet, row_num, mens, COL, new_values):
+	'''
+	Checks if cell is empty to place value, otherwise add to old value
+	'''
+	try:
+		old_value = int(sheet.cell(row_num, COL).value)
+		sheet.update_cell(row_num, COL, int(new_values) + old_value)
+	except:
+		null(sheet,mens,row_num)
+		sheet.update_cell(row_num, COL, int(new_values))
+
+
+def find_mens_row(mens, sheet):
+	'''
+	Find the correct row of a person
+	'''
+	name_list = sheet.col_values(MENS_COL)
+
+	try:
+		row_num = name_list.index(mens) + 1
+	except:
+		row_num = len(name_list) + 1
+		sheet.update_cell(row_num, MENS_COL, mens)
+
+	return row_num
+
+
+def time_stamp():
+	'''
+	Get local time, and make nice string
+	'''
+	tijd = (str(time.gmtime().tm_mday) + "/" +
+			str(time.gmtime().tm_mon) + "/" +
+			str(time.gmtime().tm_year) + ' || ' +
+			str(time.gmtime().tm_hour) + ':' +
+			str(time.gmtime().tm_min) + '.')
+	return tijd
+
+
+if __name__ == '__main__':
+	sheet = use_sheet()
+	set_up(sheet)
